f1e996ce28e62fbfc30b802807c99fb8e07e0442
Jovanna Jimenez <jovannajimenez@Jovannas-MacBook-Air.local>
2025-02-23 19:45:40 -0700

    adjusting header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ import MobileNav from './MobileNav';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <header className='bg-pink-200'>Header</header>;
+  return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>Header</header>;
 };
 
 export default Header;
