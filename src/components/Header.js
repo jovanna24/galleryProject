addccc3193d1ba2326fda07d6e5e5718aafc9641
Jovanna Jimenez <jovannajimenez@Jovannas-MacBook-Air.local>
2025-02-24 21:22:51 -0700

    finished mobileNav and header, have not started socials component

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,33 +12,34 @@ const Header = () => {
     <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
       <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
         {/*logo*/}
-        <Link to={'/'}>
+        <Link to={'/'} className='max-w-[200px]'>
           <img src={Logo} alt="" />
         </Link>
-        {/* nav initially hidden - show on desktop mode*/} 
-        <nav className='hidden lg:flex gap-x-12 font-semibold'>
-          <Link to={'/'} 
-          className="text-[#696c6d] hover:text-primary transition">
+        {/* nav initially hidden - show on desktop mode*/}
+        <nav className='hidden xl:flex gap-x-12 font-semibold'>
+          <Link to={'/'}
+            className="text-[#696c6d] hover:text-primary transition">
             Home
           </Link>
-          <Link to={'/about'} 
-          className="text-[#696c6d] hover:text-primary transition">
+          <Link to={'/about'}
+            className="text-[#696c6d] hover:text-primary transition">
             About
           </Link>
-          <Link to={'/portfolio'} 
-          className="text-[#696c6d] hover:text-primary transition">
+          <Link to={'/portfolio'}
+            className="text-[#696c6d] hover:text-primary transition">
             Portfolio
           </Link>
-          <Link to={'/contact'} 
-          className="text-[#696c6d] hover:text-primary transition">
+          <Link to={'/contact'}
+            className="text-[#696c6d] hover:text-primary transition">
             Contact
           </Link>
         </nav>
-        {/* socials*/}
-        <Socials/>
-        {/* mobile nav*/}
-        <MobileNav />
       </div>
+      {/* socials*/}
+      <Socials />
+      {/* mobile nav*/}
+      <MobileNav />
+
     </header>
   );
 };
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,19 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 // import icons 
 
-import { IoMdClose } from 'react-icons/io';
-import { CgMenuRight } from 'react-icons/cg';
+import { IoMdClose } from 'react-icons/io'; // This imports the IoMdClose icon from the react-icons/io library. This icon is typically used to represent a "close" button, likely for closing the mobile navigation menu.
+import { CgMenuRight } from 'react-icons/cg'; //imports the CgMenuRight icon from the react-icons/cg library. This icon is often used as a "hamburger" menu icon, which is used to toggle the mobile navigation menu.
 
 //import link 
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom'; //imports the Link component from the react-router-dom library; used for creating navigation links within a React Router application, allowing users to navigate between different pages without full page reloads
 
 //import motion
-import { motion } from 'framer-motion';
+import { motion, transform } from 'framer-motion'; //imports the motion component from the framer-motion library. Framer Motion is a library for creating animations in React. The motion component is used to animate elements, likely the mobile navigation menu itself
+
+const menuVariants = {
+  hidden: {
+    x: '100%'
+  },
+  show: {
+    x: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.9],
+    },
+  },
+}
 
 const MobileNav = () => {
-  return <nav>
+  const [openMenu, setOpenMenu] = useState(false);
+  return <nav className='text-primary xl:hidden'>
     {/* nav open button */}
-    </nav>;
+    <div onClick={() => setOpenMenu(true)}
+      className='text-3xl cursor-pointer'>
+      <CgMenuRight />
+    </div>
+    {/* menu */}
+    <motion.div
+      variants={menuVariants}
+      initial='hidden'
+      animate={openMenu ? 'show' : ''}
+      className='bg-white shadow-2x1 w-full absolute top-0 right-0 max-w-xs h-screen z-20'>
+      {/* icon */}
+      <div onClick={()=> setOpenMenu(false)} className='text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer'>
+        <IoMdClose />
+      </div>
+      <ul className='h-full flex flex-col justify-center items-center gap-y-8 text-primary font-primary font-bold text-3xl'>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/portfolio'>Portfolio</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
+
+      </ul>
+    </motion.div>
+  </nav>;
 };
 
 export default MobileNav;
