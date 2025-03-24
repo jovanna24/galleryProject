1739640ef93dbd117aed6b38844c562424e7a189
Jovanna Jimenez <jovannajimenez@Jovannas-MacBook-Air.local>
2025-03-23 21:25:03 -0700

    working on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,33 @@
 import React from 'react';
+//import images 
+import WomanImg from '../img/about/woman.png';
+//import link 
+import { Link } from 'react-router-dom'
 
 const About = () => {
-  return <section className='section bg-yellow-200'>About</section>;
+  return <section className='section'>
+    <div className='container mx-auto h-full relative'>
+      {/* text & img wrapper */}
+      <div className=' flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text=left lg:pt-16'>
+        {/* image */}
+        <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+          <img src={WomanImg} alt='' />
+        </div>
+        <div>
+          {/* text */}
+          <div>
+            <h1 className='h1'>About me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt quidem, eligendi necessitatibus quos quisquam illum maiores ut ad perspiciatis, quis unde, nisi rerum vel beatae numquam cupiditate commodi magni id.</p>
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae nobis vel repellat. Amet qui odit enim delectus repudiandae tenetur assumenda provident at eligendi ullam! Quod consequatur veritatis velit eveniet consectetur.</p>
+            <Link to={ '/portfolio' }> View my work</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    About
+  </section>;
 };
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return <section className='section'>
-    <div className='relative container mx-auto h-full'> 
+    <div className='container mx-auto h-full relative'>
       {/* text and image wrapper */}
       <div className='flex flex-col justify-center'>
         {/* text */}
-        <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute lg:left-0 lg:top-0 flex flex-col justify-center items-center lg:items-start'>
-          <h1 className='h1'>
+        <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1 '>
             photographer <br /> & film maker
           </h1>
           <p className='text -[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>
@@ -21,16 +21,16 @@ const Home = () => {
             hire me
           </Link>
         </div>
-      </div>
-      <div>
-        {/* image */}
-        <div className='flex justify-end'>
-          <div>
-            <img src={WomanImg} alt='' />
+
+        <div>
+          {/* image */}
+          <div className='flex justify-end max-h-96 lg:max-h-max'>
+            <div className='relative lg:=-right-40 overflow-hidden'>
+              <img src={WomanImg} alt='' />
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   </section>;
 };
